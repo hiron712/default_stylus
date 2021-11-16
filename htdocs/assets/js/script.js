6bbc6326cfb37d499acde115d6842329b8d99c03
hiroyuki <nagata@petabit.co.jp>
2021-11-16 13:20:55 +0900

    更新

--- htdocs/assets/js/script.js
+++ htdocs/assets/js/script.js
@@ -42,6 +42,62 @@
       return false;
     });
 
+
+    topSlick();
+
+    // ウインドウがリサイズする度にチェック
+    $(window).on('resize orientationchange',function(){
+      carousel();
+    });
+    carousel();
   });
 
+
+  function topSlick(){
+    var slider;
+    slider = $('#main-img .slide-area');
+    slider.slick({
+      dots: true,
+      autoplay: false,
+      arrows: true,
+      infinite: true,
+      // prevArrow:'<div class="prev"><img src="/wp/wp-content/themes/labbrains/assets/images/icon/icon_prev.svg"></div>',
+      // nextArrow:'<div class="next"><img src="/wp/wp-content/themes/labbrains/assets/images/icon/icon_next.svg"></div>',
+      autoplaySpeed: 8000,
+      speed: 600,
+      lazyLoad:"progressive",
+      // centerMode: true,
+      centerPadding: '10%',
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      swipeToSlide: true
+    });
+  }
+
+  function carousel(){
+
+    if($('#header .menu').css('display') === 'none'){
+      var slider;
+      slider = $('#main .carousel');
+      slider.slick({
+        dots: true,
+        autoplay: false,
+        arrows: true,
+        infinite: true,
+        // prevArrow:'<div class="prev"><img src="/wp/wp-content/themes/labbrains/assets/images/icon/icon_prev.svg"></div>',
+        // nextArrow:'<div class="next"><img src="/wp/wp-content/themes/labbrains/assets/images/icon/icon_next.svg"></div>',
+        autoplaySpeed: 8000,
+        speed: 600,
+        lazyLoad:"progressive",
+        // centerMode: true,
+        centerPadding: '10%',
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        swipeToSlide: true
+      });
+    }else{
+      $('#main .carousel').slick('unslick');
+    }
+
+  }
 })(jQuery);
